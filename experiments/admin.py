0a2c34e18ae221a2e9298e95e01cf547160de923
Mustafa Khattab <mustafa.khattab@gmail.com>
2013-10-31 10:19:30 +0200

    Register models in admin

--- experiments/admin.py
+++ experiments/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
-# Register your models here.
+from experiments.models import (Experiment,
+                                Participant,
+                                Session,
+                                Event,
+                                Item,
+                                ChoiceSet,
+                                Winner)
+
+
+class ExperimentAdmin(admin.ModelAdmin):
+    pass
+
+
+class ParticipantAdmin(admin.ModelAdmin):
+    pass
+
+
+class SessionAdmin(admin.ModelAdmin):
+    pass
+
+
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
+
+class ChoiceSetAdmin(admin.ModelAdmin):
+    pass
+
+
+class WinnerAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Experiment, ExperimentAdmin)
+admin.site.register(Participant, ParticipantAdmin)
+admin.site.register(Session, SessionAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(ChoiceSet, ChoiceSetAdmin)
+admin.site.register(Winner, WinnerAdmin)
