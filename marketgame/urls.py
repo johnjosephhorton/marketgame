923f07afa788c8a614e8db41bda39024788c9d21
Mustafa Khattab <mustafa.khattab@gmail.com>
2013-10-31 19:47:51 +0200

    Initial experiment template

--- marketgame/urls.py
+++ marketgame/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+    url(r'^exp/', include('experiments.urls', 'exp', 'experiments')),
     url(r'^admin/', include(admin.site.urls)),
 )
